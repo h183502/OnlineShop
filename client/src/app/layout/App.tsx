85e9f4972a56b686821b1d377f980d5e2f3a218a
h183502 <heine.fjeld@gmail.com>
2022-09-26 14:49:44 +0200

    Errorhandling

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,13 +1,17 @@
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { Container } from "@mui/system";
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import AboutPage from "../../features/about/AboutPage";
 import Catalog from "../../features/catalog/Catalog";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import ContactPage from "../../features/contact/ContactPage";
 import HomePage from "../../features/home/HomePage";
 import Header from "./Header";
+import 'react-toastify/dist/ReactToastify.css'; 
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,17 +31,22 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position='bottom-right' hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="catalog/:id" element={<ProductDetails />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="contact" element={<ContactPage />} />
-        </Routes>
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route path={'/(.+)'} render={() => (
+        <Container>
+          <Switch>
+            <Route exact path="/catalog" component={Catalog} />
+            <Route path="/catalog/:id" component={ProductDetails} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/contact" component={ContactPage} />
+            <Route path="/server-error" component={ServerError} />
+            <Route component={NotFound} />
+          </Switch>
+        </Container>
+      )}></Route>
     </ThemeProvider>
   );
 }
